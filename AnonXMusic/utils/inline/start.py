77f21d90526d03a476dd072e1c2c42ae036417b2
Zabuza1004 <154702035+Zabuza1004@users.noreply.github.com>
2024-01-25 12:51:09 +0530

    Update start.py
    Signed-off-by: Zabuza1004 <154702035+Zabuza1004@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,6 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=f"https://telegra.ph/file/a5b8092d4ad9b9240d179.mp4"),
-        ],
+            
     ]
     return buttons
